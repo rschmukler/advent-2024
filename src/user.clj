4f22ff465cf36ea5a407559fd5048d7749b435b9
Ryan Schmukler <rschmukler@gmail.com>
2024-12-01 17:55:42 -0600

    feat: clerk build comment
    Add a comment to build everything via clerk to generate pages.

--- src/user.clj
+++ src/user.clj
@@ -4,3 +4,8 @@
 (clerk/serve! {:browse      true
                :paths       ["src/aoc/*"]
                :watch-paths ["src/aoc/"]})
+
+(comment
+  (clerk/build!
+    {:paths  ["src/aoc/*.clj"]
+     :browse true}))
