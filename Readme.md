3cf99543487e15525180ca72363509f0ac479755
Ryan Schmukler <rschmukler@gmail.com>
2024-12-01 18:14:11 -0600

    feat: aoc puzzle input http fetch
    Add support for fetching puzzle input via HTTP

--- Readme.md
+++ Readme.md
@@ -5,4 +5,6 @@ Another year of Advent of Code! This time we will be using... Clojure!
 
 # Puzzle Input
 
-To solve, place your puzzle input under `resources/day_x/input.txt`
+To solve, place your puzzle input under `resources/day_x/input.txt`.
+Alternatively, if you set an environment variable of `AOC_SESSION_ID` from your
+`session=*` in the AOC Cookie, the `aoc/input` function will automatically fetch it for you.
--- src/aoc.clj
+++ src/aoc.clj
@@ -1,6 +1,8 @@
 (ns aoc
   "Namespace utility for common functions for solving AOC"
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [hato.client :as http]
+            [clojure.java.io :as io]))
 
 (def ^:private -process-input-docs
   "Optionally takes the following arguments:
@@ -24,6 +26,21 @@
       true        (map parse-line)
       (not lines) (first))))
 
+(def ^:private -AOC-SESSION-ENV-VAR
+  "The name of the environment variable that should contain the Advent of Code session ID."
+  "AOC_SESSION_ID")
+
+(defn- -fetch-remote-input
+  "Utility function to fetch the input for the given day from the Acvent of Code website."
+  ([day]
+   (-fetch-remote-input day (.getYear (java.time.LocalDate/now))))
+  ([day year]
+   (let [session-id (System/getenv -AOC-SESSION-ENV-VAR)]
+     (assert session-id (format "%s environment variable not set" -AOC-SESSION-ENV-VAR))
+     (-> (http/get (str "https://adventofcode.com/" year "/day/" day "/input")
+                   {:headers {"Cookie" (str "session=" session-id)}})
+         :body))))
+
 (defn input
   {:doc
    (str
@@ -32,8 +49,25 @@
    :arglists
    (list (into '[day] (-> #'process-input meta :arglists first rest)))}
   [day & {:as opts}]
-  (-> (slurp (str "resources/day_" day "/input.txt"))
-      (process-input opts)))
+  (let [file-path (str "resources/day_" day "/input.txt")
+        f         (io/file file-path)]
+    (-> (if (.exists f)
+          (slurp f)
+          (let [_    (when-not (System/getenv "AOC_SESSION_ID")
+                       (throw (ex-info
+                                (str/join
+                                  "\n"
+                                  ["Attempted to open missing puzzle input."
+                                   (format "Please either set %s or provide the input file in resources/day_%d/input.txt"
+                                           -AOC-SESSION-ENV-VAR
+                                           day)])
+                                {:day  day
+                                 :path file-path})))
+                _    (println "Fetching data for day" day)
+                data (-fetch-remote-input day)]
+            (spit f data)
+            data))
+        (process-input opts))))
 
 (defn example
   {:doc
